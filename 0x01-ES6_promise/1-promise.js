eeac07a6430ba272a94ebff4c5f887ab8370615a
milkawinoh <milkawinoh@gmail.com>
2024-05-03 18:58:03 +0300

    task

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,12 @@
-// Exporting a function named getFullResponseFromAPI
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success === true) {
-      const obj = { status: 200, body: 'Success' };
-      resolve(obj); // Resolve the Promise with the success object
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
     } else {
-      const errorMessage = 'The fake API is not working currently';
-      reject(errorMessage); // Reject the Promise with an error message
+      reject(new Error('The fake API is not working currently'));
     }
   });
 }
